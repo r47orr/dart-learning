1a6e324d1ba4f7ff96aaeb55b8fc329a57080815
r47orr <razoroller529@gmail.com>
2023-10-24 17:06:08 -0300

    update: main function logic, still wip
    * interrupting logic still needs fixing, but can't do so in main function

--- bin/bmi_calculator.dart
+++ bin/bmi_calculator.dart
@@ -5,6 +5,7 @@ import 'dart:io';
 void main(List<dynamic> enforce) {
   var p1 = Person();
   var bmiCalculator = Calculator();
+  bool res = true;
 
   if (enforce.isEmpty) {
     try {
@@ -14,11 +15,15 @@ void main(List<dynamic> enforce) {
     } catch (e) {
       print('Invalid name passed. Please, submit a string name.');
 
+      res = false;
+
       main([]);
     }
   }
 
-  if (enforce.isEmpty) {
+  print('execução: $res \n');
+
+  if (enforce.length > 1 && res == true) {
     try {
       print(
           'Please enter in ${p1.getName()}\'s weight (in killograms, i.e: 40): ');
@@ -28,25 +33,46 @@ void main(List<dynamic> enforce) {
       print(
           'Invalid weight passed. Please, submit a number of type double for weight.');
 
+      res = false;
+
       main([p1.getName()]);
     }
   }
 
-  if (enforce.isEmpty) {
+  print('execução: $res \n');
+
+  if (enforce.length > 2 && res == true) {
     try {
       print(
           'Please enter in ${p1.getName()}\'s height (in meters, i.e: 1.55 | NOT 155): ');
       double? height = double.parse(stdin.readLineSync()!);
       if (height > 100) {
+        res = false;
+
         throw (ArgumentError(
             'Invalid height format. Use meters instead of centimeters.'));
+      } else {
+        p1.setHeight(height);
       }
-      p1.setHeight(height);
     } catch (e) {
       print(
           'Invalid height passed. Please, submit a number of type double for height.');
+
+      res = false;
+
+      main([p1.getName(), p1.getWeight()]);
     }
   }
+
+  print('execução: $res \n');
+
+  if (res == false) {
+    print('interromper execução');
+
+    return;
+  }
+
+  print('execução: $res \n');
 
   print(
       '${p1.getName()}\'s BMI is ${bmiCalculator.classify(p1.getWeight(), p1.getHeight())}');
